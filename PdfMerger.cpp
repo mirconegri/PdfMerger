777b9ce21dbbd88644bd1d004129440d3d55788e
Mirco Negri <79650090+mirconegri@users.noreply.github.com>
2025-12-13 07:43:50 +0100

    Update PdfMerger.cpp

--- PdfMerger.cpp
+++ PdfMerger.cpp
@@ -6,11 +6,7 @@ using namespace PoDoFo;
 using namespace std;
 
 void PdfMerger::addFile(const string& path) {
-    // Check whether the given file path exists on the filesystem.
-    // If it doesn't, print an error ";
-        return;
-    
-
+    // Check whether the given 
     try {
         // Create an empty PDF document that will contain the merged result.
         PdfMemDocument outputDoc;
